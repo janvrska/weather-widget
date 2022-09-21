4c1db00c9ac9f7da7a045133868c8dc3daad41bb
Jan Vrska <1644599+janvrska@users.noreply.github.com>
2022-09-21 23:04:41 +0200

    init commit, ImGui/GLFW window setup

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <cstdio>
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char *description) {
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main() {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+    glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(300, 300, "Weather widget", NULL, NULL);
+    glfwSetWindowPos(window, 500, 500);
+
+    if (window == NULL)
+        return 1;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImVec4 clearColor = ImVec4{0.0f, 0.0f, 0.0f, 0.0f};
+    bool open = true;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove;
+        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4{0, 0, 0, 0});
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4{0, 0, 0, 0.5f});
+
+        ImGui::Begin("widget", &open, flags);
+        ImGui::SetWindowSize(ImVec2{300, 300});
+        ImGui::SetWindowPos(ImVec2{0, 0});
+        ImGui::Text("Test");
+        ImGui::PopStyleColor(2);
+        ImGui::End();
+
+        ImGui::Render();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clearColor.x * clearColor.w, clearColor.y * clearColor.w, clearColor.z * clearColor.w,
+                     clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
